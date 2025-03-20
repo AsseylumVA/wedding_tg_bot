313d59ac8adc9944cace3fa4f26500cdbe674cba
rodion <rodion@flexites.org>
2025-03-20 18:07:47 +0500

    finish message sender

--- managers/message_sender.py
+++ managers/message_sender.py
@@ -1,5 +1,4 @@
-from aiogram.methods import SendMessage
-
+from create_bot import bot
 from .redis_mgr import RedisManager
 
 
@@ -8,7 +7,7 @@ class MessageSenderManger:
         self.redis_manager = RedisManager()
 
     async def send_messages(self, message_text):
-        user_keys = self.redis_manager.get_all_users()
+        user_keys = await self.redis_manager.get_all_users()
         for user_key in user_keys:
-            user_id = user_key.replace('user:')
-            await SendMessage(chat_id=user_id, text = message_text)
+            user_id = user_key.replace('user:', '')
+            await bot.send_message(chat_id=user_id, text = message_text)
