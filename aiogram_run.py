55b7dd8f3caee73adcc30cc4bedbb0bf864de224
rodion <rodion@flexites.org>
2025-03-24 12:09:06 +0500

    set default settings whe bot created

--- aiogram_run.py
+++ aiogram_run.py
@@ -2,12 +2,19 @@ import asyncio
 
 from create_bot import bot, dp
 from handlers import admin_handlers, user_handlers
+from managers.redis_mgr import RedisManager
 
 
 async def main():
     dp.include_routers(user_handlers.router, admin_handlers.router)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+    await set_settings()
+
+
+async def set_settings():
+    redis_manager = RedisManager()
+    await redis_manager.set_def_settings()
 
 
 if __name__ == '__main__':
