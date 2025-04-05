e41b05b75f99818aff6aaa09cd06e04864c1d34a
yin <skykanade@gmal.com>
2025-04-05 17:02:11 +0500

    rework messages sending

--- managers/message_sender.py
+++ managers/message_sender.py
@@ -32,7 +32,7 @@ class MessageSenderManager:
                 user_id = user_key.replace('user:', '')
                 await self.bot.send_message(chat_id=user_id, text=message_text,
                                             reply_markup=reply_markup)
-                await asyncio.sleep(0.1)  # Anti-flood
+                await asyncio.sleep(0.3)  # Anti-flood
             except Exception as e:
                 logger.error(f"Ошибка отправки для {user_id}: {e}")
 
@@ -40,11 +40,17 @@ class MessageSenderManager:
         now = await get_current_time()
         for msg_name, msg_data in settings.SCHEDULED_MESSAGES.items():
             if msg_data['send_time'] <= now:
-                await self.bot.send_message(
-                    chat_id=user_id,
-                    text=msg_data['text'],
-                    reply_markup=msg_data['reply_markup']
-                )
+                try:
+                    await self.bot.send_message(
+                        chat_id=user_id,
+                        text=msg_data['text'],
+                        reply_markup=msg_data['reply_markup']
+                    )
+                    await asyncio.sleep(0.3)  # Anti-flood
+                except Exception as e:
+                    logger.error(
+                        f"Ошибка отправки {msg_name} для {user_id}: {str(e)}")
+                    continue
 
 
 class MessageScheduler:
@@ -58,15 +64,18 @@ class MessageScheduler:
         """Проверяет и отправляет запланированные сообщения"""
         logger.info('Рассылка начата')
         messages_to_send = settings.SCHEDULED_MESSAGES.copy()
-        while True:
+        while messages_to_send:
             now = await get_current_time()
-            for msg_name, msg_data in messages_to_send.items():
+            for msg_name, msg_data in list(messages_to_send.items()):
                 if msg_data['send_time'] <= now:
                     await self.sender.send_messages(msg_data['text'],
                                                     msg_data['reply_markup'])
                     logger.info(f'Сообщение отправлено: {msg_name}')
                     messages_to_send.pop(msg_name)
+                    # Задержка для защиты от флуда
+                    await asyncio.sleep(0.3)
             await asyncio.sleep(60)
+        logger.info('Все запланированные сообщения отправлены')
 
     async def run(self):
         """Запускает все задачи рассылки"""
